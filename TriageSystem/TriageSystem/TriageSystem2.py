9c110921f41abedcf4410d483b06df000a10a41a
Ben Ward <34252146+iwardy18@users.noreply.github.com>
2023-01-11 16:26:16 +0000

    Latest

--- TriageSystem/TriageSystem/TriageSystem2.py
+++ TriageSystem/TriageSystem/TriageSystem2.py
@@ -2,28 +2,24 @@
 ## if a person needs to see a GP
 
 # import utilities
-from Utilities import cls, close, splash, countDown,openPage;
+from Utilities import cls, close, splash, countDown;
 
 
 #start the splash screen
 splash();
 
 #start the program.
-print("Welcome to the self triage COVID-19 application!");#.center(50,'#'));
+print("Welcome to the self triage COVID-19 application!");
 print("\n");
 print("We will ask a few questions to determin if you should see your GP.");
 print("\n");
-#openPage("https://google.com/");
 
-#label .happy;
 happy = input("Are you happy to continue? y/n: ");
 if (happy.capitalize() == 'Y' or happy.capitalize() == "YES"):
   #question1:  
     cls();
     name = input("What is your name? ");
     cls();
-    #age = input(name + ", what is your age?");
-    #cls();  
     cough = input("Hi " + name + ", Do you have a regular cough? y/n: ");
     if (cough.capitalize() == 'Y' or cough.capitalize() == "YES"):
         cls();
